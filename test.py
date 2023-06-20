b63927196c1fd95fb0bceb46c4d789d859e7dacf
KeinsWolfi <80259183+KeinsWolfi@users.noreply.github.com>
2023-06-20 21:09:16 +0200

    Update test.py
    Made the timezone calculation actually work

--- test.py
+++ test.py
@@ -15,23 +15,29 @@ uuid=str(MojangReq['id'])
 
 
 data = req.get("https://api.hypixel.net/player?key=27477778-1002-491b-ab71-0a11adeb8912&uuid="+uuid).json()
+#recentgames = req.get("https://api.hypixel.net/recentgames?key=27477778-1002-491b-ab71-0a11adeb8912&uuid="+uuid).json()
 
-timezone = int(input("Enter your timezone in UTC (put - if negative) >> UTC+"))
 
-firstLoginUnix = data['player']['firstLogin']
-firstLoginP = datetime.utcfromtimestamp(float((firstLoginUnix+timezone*3600)/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
-lastLoginUnix = data['player']['lastLogin']
-lastLoginP = datetime.utcfromtimestamp(float((lastLoginUnix+timezone*3600)/1000)).strftime('%Y-%m-%d %H:%M:%S')
+timezone = input("Enter your timezone in UTC (put - if negative) >> UTC+")
 
-lastLogoutUnix = data['player']['lastLogout']
-lastLogoutP = datetime.fromtimestamp(float((lastLogoutUnix+timezone*3600)/1000)).strftime('%Y-%m-%d %H:%M:%S')
+firstLoginUnixw = data['player']['firstLogin']
+firstLoginUnix = int(firstLoginUnixw)+int(timezone)*3600000
+firstLoginP = datetime.utcfromtimestamp(float(firstLoginUnix/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
+lastLoginUnixw = data['player']['lastLogin']
+lastLoginUnix = int(lastLoginUnixw)+int(timezone)*3600000
+lastLoginP = datetime.utcfromtimestamp(float(lastLoginUnix/1000)).strftime('%Y-%m-%d %H:%M:%S')
+
+lastLogoutUnixw = data['player']['lastLogout']
+lastLogoutUnix = int(lastLogoutUnixw)+int(timezone)*3600000
+lastLogoutP = datetime.utcfromtimestamp(float(lastLogoutUnix/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
 
 #print(data)
+#print(recentgames)
 print("PlayerKarma: "+str(data['player']['karma']))
-print("First Login:"+str(firstLoginP))
-print("Last Login:"+str(lastLoginP))
-print("Last Logout:"+str(lastLogoutP))
+print("First Login: "+str(firstLoginP))
+print("Last Login: "+str(lastLoginP))
+print("Last Logout: "+str(lastLogoutP))
 input("Press ENTER to exit")
