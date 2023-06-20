d403f82b7c82666ab0de45a68acd7bbb0a736dcc
KeinsWolfi <80259183+KeinsWolfi@users.noreply.github.com>
2023-06-20 18:59:11 +0200

    Update test.py
    Removed more unneeded code

--- test.py
+++ test.py
@@ -7,16 +7,12 @@ from datetime import datetime
 userinput = str(input("Enter the username you want to request- "))
 
 
-#get uuid and save it to a var#
+#get uuid and save it to a var
 
 MojangReq= req.get("https://api.mojang.com/users/profiles/minecraft/"+userinput).json()
 uuid=str(MojangReq['id'])
 
-#apikey
 
-f=open('apikey.txt', 'r+')
-apikey=f.read()
-f.close()
 
 data = req.get("https://api.hypixel.net/player?key=27477778-1002-491b-ab71-0a11adeb8912&uuid="+uuid).json()
 
