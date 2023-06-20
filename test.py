98bc56efc21b8ccd75e796e33e68bf932832a1ee
KeinsWolfi <80259183+KeinsWolfi@users.noreply.github.com>
2023-06-20 15:53:34 +0200

    Update test.py
    Added First and Last login retriever

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests as req
 import mojang
 import time
+from datetime import datetime
 
 
 userinput = str(input("Enter the username you want to request- "))
@@ -19,6 +20,15 @@ f.close()
 
 data = req.get("https://api.hypixel.net/player?key=27477778-1002-491b-ab71-0a11adeb8912&uuid="+uuid).json()
 
+firstLoginUnix = data['player']['firstLogin']
+firstLoginP = datetime.utcfromtimestamp(float(firstLoginUnix/1000)).strftime('%Y-%m-%d %H:%M:%S')
+
+lastLoginUnix = data['player']['lastLogin']
+lastLoginP = datetime.utcfromtimestamp(float(lastLoginUnix/1000)).strftime('%Y-%m-%d %H:%M:%S')
+
+
 #print(data)
 print("PlayerKarma: "+str(data['player']['karma']))
+print("First Login:"+str(firstLoginP)+"  (You may have to add or subtract some hours depending on your timezone, as this date is in UTC)")
+print("Last Login:"+str(lastLoginP)+"  (You may have to add or subtract some hours depending on your timezone, as this date is in UTC)")
 input("Press ENTER to exit")
